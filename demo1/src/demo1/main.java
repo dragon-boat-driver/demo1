5a8be7a4f720d28946236351b608f95a1820764e
moshangyilvguhun <1322360038@qq.com>
2017-05-18 12:55:19 +0800

    testgit

---
+++ demo1/src/demo1/main.java
@@ -0,0 +1,11 @@
+package demo1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("java 1.9.0_131");
+		System.out.println("test_git");
+	}
+
+}
